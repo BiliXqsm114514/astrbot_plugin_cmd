265b3a65271e20b65a6b81a4d5985c3b93efbf88
BiliXqsm114514 <3372900648@qq.com>
2025-04-13 17:07:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-import os
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.core.provider.register import llm_tools
+from astrbot.api.event.filter import command,PermissionType
+import os
+from astrbot.api import llm_tool, logger
+from astrbot.api.event import filter, AstrMessageEvent
 
 
-@register("astrbot_plugin_cmd", "BiliXqsm114514", "让ai调用本地cmd", "0.0.1", "repo url")
-class CmdUsing(Star):
+@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+class Main(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
+    @command("cmd_use_help")
+    async def help(self,event: AstrMessageEvent):
+        yield event.plain_result("ai会自动调用cmd")
+        ...
+    
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @llm_tools(name="cmd_using")
-    async def cmd_using(self, event: AstrMessageEvent,content:str):
-        '''使用本地 cmd 指令'''  # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
-        exit_code = os.system(content)
+    @command("cmd_use")
+    async def cmd_use(self, event: AstrMessageEvent):
+        """简陋的cmd调用插件"""
+        ...
+
+
+    @filter.permission_type(PermissionType.ADMIN)
+    @llm_tool(name="cmd_use")  # 如果 name 不填，将使用函数名
+    async def cmd_use(self, event: AstrMessageEvent, command: str) -> MessageEventResult:
+        '''使用本地cmd命令行。
+
+        Args:
+            command(string): 命令
         '''
-        user_name = event.get_sender_name()
-        message_str = event.message_str  # 获取消息的纯文本内容
-        yield event.plain_result(f"Hello, {user_name}!")  # 发送一条纯文本消息
-        '''
-        return "指令成功" if exit_code == 0 else "出现报错"
+        if os.system(command) == 0:
+            resp = "成功"
+        else:
+            resp = "失败"
+        return "指令" + resp
+        #yield event.plain_result("指令: " + resp)
 
     async def terminate(self):
-        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
+        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        ...
